0dbb14e0e6cc619708a8ab6466f668f5deea19a5
jackietana <213204740+jackietana@users.noreply.github.com>
2026-06-23 10:47:04 +0300

    minor updates in repo methods

--- auth-service/internal/repository/auth.go
+++ auth-service/internal/repository/auth.go
@@ -15,18 +15,18 @@ func NewRepository(db *sql.DB) *Repository {
 	return &Repository{db}
 }
 
-func (r *Repository) CreateUser(ctx context.Context, usr domain.User) (string, error) {
+func (r *Repository) CreateUser(ctx context.Context, email, passwordHash string) (string, error) {
 	var id string
 	strExec := "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id"
 
-	if err := r.db.QueryRowContext(ctx, strExec, usr.Email, usr.Password).Scan(&id); err != nil {
+	if err := r.db.QueryRowContext(ctx, strExec, email, passwordHash).Scan(&id); err != nil {
 		return "", err
 	}
 
 	return id, nil
 }
 
-func (r *Repository) GetUser(ctx context.Context, email string) (domain.User, error) {
+func (r *Repository) GetUserByEmail(ctx context.Context, email string) (domain.User, error) {
 	var usr domain.User
 	strExec := "SELECT id, email, password, created_at FROM users WHERE email=$1"
 
