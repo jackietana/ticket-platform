15dc91f043b096fcf78eaa469c43e1c6bd724a02
jackietana <213204740+jackietana@users.noreply.github.com>
2026-06-23 09:51:19 +0300

    update dto models

--- auth-service/internal/domain/user.go
+++ auth-service/internal/domain/user.go
@@ -8,14 +8,9 @@ import (
 
 type User struct {
 	ID        uuid.UUID `json:"id"`
-	Email     string    `json:"email" binding:"required,email"`
-	Password  string    `json:"password" binding:"required,gte=6"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
-}
-
-type UserInput struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,gte=6"`
 }
 
 type Session struct {
