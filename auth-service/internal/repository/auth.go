c9ce788ccc87e6188251c1b8e5bafd0f3b52f369
jackietana <213204740+jackietana@users.noreply.github.com>
2026-06-23 09:56:56 +0300

    update repo methods

--- auth-service/internal/repository/auth.go
+++ auth-service/internal/repository/auth.go
@@ -15,19 +15,22 @@ func NewRepository(db *sql.DB) *Repository {
 	return &Repository{db}
 }
 
-func (r *Repository) CreateUser(ctx context.Context, usr domain.User) error {
-	strExec := "INSERT INTO users (email, password) VALUES ($1, $2)"
-	if _, err := r.db.ExecContext(ctx, strExec, usr.Email, usr.Password); err != nil {
-		return err
+func (r *Repository) CreateUser(ctx context.Context, usr domain.User) (string, error) {
+	var id string
+	strExec := "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id"
+
+	if err := r.db.QueryRowContext(ctx, strExec, usr.Email, usr.Password).Scan(&id); err != nil {
+		return "", err
 	}
 
-	return nil
+	return id, nil
 }
 
-func (r *Repository) GetUser(ctx context.Context, inp domain.UserInput) (domain.User, error) {
+func (r *Repository) GetUser(ctx context.Context, email string) (domain.User, error) {
 	var usr domain.User
-	strExec := "SELECT id, email, password, created_at FROM users WHERE email=$1 AND password=$2"
-	err := r.db.QueryRowContext(ctx, strExec, inp.Email, inp.Password).
+	strExec := "SELECT id, email, password, created_at FROM users WHERE email=$1"
+
+	err := r.db.QueryRowContext(ctx, strExec, email).
 		Scan(&usr.ID, &usr.Email, &usr.Password, &usr.CreatedAt)
 	if err != nil {
 		return domain.User{}, err
