e25028a2c1d1c618462e970e18fe1a5351a25abe
jackietana <213204740+jackietana@users.noreply.github.com>
2026-07-05 11:16:20 +0300

    add auth middleware

--- catalog-service/internal/transport/rest/v1/handler.go
+++ catalog-service/internal/transport/rest/v1/handler.go
@@ -2,11 +2,14 @@ package rest
 
 import (
 	"context"
+	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/jackietana/ticket-platform/catalog-service/internal/domain"
 	"github.com/jackietana/ticket-platform/catalog-service/internal/dto"
+	pb "github.com/jackietana/ticket-platform/catalog-service/internal/transport/grpc/gen"
 )
 
 type CatalogService interface {
@@ -16,20 +19,22 @@ type CatalogService interface {
 }
 
 type Handler struct {
-	service CatalogService
+	authClient pb.AuthServiceClient
+	service    CatalogService
 }
 
-func NewHandler(service CatalogService) *Handler {
-	return &Handler{service}
+func NewHandler(client pb.AuthServiceClient, service CatalogService) *Handler {
+	return &Handler{authClient: client, service: service}
 }
 
-func (h *Handler) Init() *gin.Engine {
+func (h *Handler) InitRouter() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger(), gin.Recovery())
 
 	v1 := router.Group("/api/v1")
 	{
 		events := v1.Group("/events")
+		events.Use(h.AuthMiddleware())
 		{
 			events.GET("/:id", h.GetEventByID)
 			events.GET("", h.ListEvents)
@@ -38,4 +43,44 @@ func (h *Handler) Init() *gin.Engine {
 	}
 
 	return router
+}
+
+func (h *Handler) AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		authValues := strings.Split(authHeader, " ")
+
+		if len(authValues) != 2 {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, dto.ErrorResponse{Error: "invalid auth header"})
+			return
+		}
+
+		if authValues[0] != "Bearer" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, dto.ErrorResponse{Error: "invalid auth header"})
+			return
+		}
+
+		if len(authValues[1]) == 0 {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, dto.ErrorResponse{Error: "invalid auth token"})
+			return
+		}
+
+		c.Set("client_ip", c.ClientIP())
+		c.Set("user-agent", c.GetHeader("User-Agent"))
+
+		resp, err := h.authClient.ValidateToken(c.Request.Context(), &pb.ValidateTokenRequest{Token: authValues[1]})
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, dto.ErrorResponse{Error: "token is invalid"})
+			return
+		}
+
+		if !resp.IsValid {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, dto.ErrorResponse{Error: "token is expired"})
+			return
+		}
+
+		c.Set("user_id", resp.UserId)
+
+		c.Next()
+	}
 }
