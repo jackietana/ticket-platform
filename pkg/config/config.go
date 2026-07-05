330f5eb1dddc3efffee0049ec80f3ec74233bda1
jackietana <213204740+jackietana@users.noreply.github.com>
2026-07-05 15:52:03 +0300

    move common config structs and methods

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,5 +1,10 @@
 package pkgConfig
 
+import (
+	"fmt"
+	"os"
+)
+
 type ServerConfig struct {
 	GRPCPort string `yaml:"grpc_port"`
 	RESTPort string `yaml:"rest_port"`
@@ -12,4 +17,21 @@ type PostgresConfig struct {
 	User    string
 	Pass    string
 	SSLMode string
+}
+
+func NewPostgresConfig() PostgresConfig {
+	cfg := PostgresConfig{}
+
+	cfg.Host = os.Getenv("APP_DB_HOST")
+	cfg.Name = os.Getenv("APP_DB_NAME")
+	cfg.User = os.Getenv("APP_DB_USER")
+	cfg.Pass = os.Getenv("APP_DB_PASS")
+	cfg.SSLMode = os.Getenv("APP_DB_SSLMODE")
+
+	return cfg
+}
+
+func (c *PostgresConfig) GetDatabaseConnString() string {
+	return fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		c.Host, c.Port, c.User, c.Name, c.Pass, c.SSLMode)
 }
