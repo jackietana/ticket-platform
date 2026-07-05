ff792a448e95f52bbe9c3898ab2c2b5695bdeaef
jackietana <213204740+jackietana@users.noreply.github.com>
2026-07-05 15:53:16 +0300

    move common psql conn file

---
+++ pkg/database/psql/psql.go
@@ -0,0 +1,68 @@
+package psql
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	pkgConfig "github.com/jackietana/ticket-platform/pkg/config"
+	_ "github.com/lib/pq"
+)
+
+var (
+	ErrNoNewMigrations = errors.New("no change")
+)
+
+func NewPostgresConnection(cfg *pkgConfig.PostgresConfig) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.GetDatabaseConnString())
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func RunUpMigrations(cfg *pkgConfig.PostgresConfig, service string) error {
+	migrationsPath := os.Getenv("APP_MIGRATIONS_PATH")
+	if migrationsPath == "" {
+		_, b, _, _ := runtime.Caller(0)
+		migrationsPath = filepath.Join(filepath.Dir(b), "../../../initdb/migrations/", service)
+	}
+	migrationDir := "file://" + migrationsPath
+
+	db, err := sql.Open("postgres", cfg.GetDatabaseConnString())
+	if err != nil {
+		return fmt.Errorf("failed to connect to db: %w", err)
+	}
+	defer db.Close()
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("failed to create driver: %w", err)
+	}
+	defer driver.Close()
+
+	migrateInst, err := migrate.NewWithDatabaseInstance(migrationDir, "postgres", driver)
+	if err != nil {
+		return fmt.Errorf("failed to create migrate instance: %w", err)
+	}
+
+	if err := migrateInst.Up(); err != nil {
+		if errors.Is(err, ErrNoNewMigrations) {
+			return fmt.Errorf("failed to up db: %w", err)
+		}
+	}
+
+	migrateInst.Close()
+	return nil
+}
